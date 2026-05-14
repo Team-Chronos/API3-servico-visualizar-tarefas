d25a713774aa3fad701398faa2294605f15ebdaa
Rafael Sette de Araujo <138353834+Sette0o0@users.noreply.github.com>
2026-05-14 11:30:54 -0300

    fix (#137): caminho redundante

--- visualizarTarefas/src/main/java/com/api/visualizarTarefas/controle/controleItem.java
+++ visualizarTarefas/src/main/java/com/api/visualizarTarefas/controle/controleItem.java
@@ -1,9 +1,15 @@
 package com.api.visualizarTarefas.controle;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.api.visualizarTarefas.modelo.Item;
 import com.api.visualizarTarefas.servico.servicoItem;
 
@@ -15,17 +21,17 @@ public class controleItem {
     @Autowired
     private servicoItem servicoItem;
 
-    @GetMapping("/itens/tarefa/{tarefaId}")
+    @GetMapping("/tarefa/{tarefaId}")
     public ResponseEntity<List<Item>> listarItensPorTarefa(@PathVariable Long tarefaId) {
         return ResponseEntity.ok(servicoItem.listarItensPorTarefa(tarefaId));
     }
 
-    @GetMapping("/itens/projeto/{projetoId}")
+    @GetMapping("/projeto/{projetoId}")
     public ResponseEntity<List<Item>> listarItensPorProjeto(@PathVariable Long projetoId) {
         return ResponseEntity.ok(servicoItem.listarItensPorProjeto(projetoId));
     }
 
-    @GetMapping("/itens/projeto/{projetoId}/responsavel/{responsavelId}")
+    @GetMapping("/projeto/{projetoId}/responsavel/{responsavelId}")
     public ResponseEntity<List<Item>> listarItensPorProjetoEResponsavel(
             @PathVariable Long projetoId, @PathVariable Long responsavelId) {
         return ResponseEntity.ok(servicoItem.listarItensPorProjetoEResponsavel(projetoId, responsavelId));
